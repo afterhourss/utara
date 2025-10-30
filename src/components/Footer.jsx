ed91a25b1019cc352b5b1140eb09700363209285
afterhourss <adiptabagasss@gmail.com>
2025-10-30 13:56:05 +0700

    adding site config

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,29 @@
-import { FaInstagram } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { IoLogoGithub } from "react-icons/io";
+import { siteConfig } from "../config/site";
 
 export default function Footer() {
   return (
     <div className="flex text-[#9C9C9C] gap-10 items-center justify-between mt-20 py-6">
-        <div className="hidden md:block">Personal site</div>
-        <div className="flex gap-3 items-center">
-          <span class="relative flex h-3 w-3">
-            <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-            <span class="relative inline-flex rounded-full h-3 w-3 bg-green-700"></span>
-          </span>
-          <div>All system normal</div>
-        </div>
-        <div className="flex items-center text-3xl gap-5">
-          <a href="https://www.instagram.com/nrthgas.0/" target="_blank"><FaInstagram/></a>
-          <a href="https://www.linkedin.com/in/adiptabagas/" target="_blank"><FaLinkedin /></a>
-          <a href="https://github.com/afterhourss" target="_blank"><IoLogoGithub/></a> 
-        </div>
+      <div className="hidden md:block">Personal site</div>
+      <div className="flex gap-3 items-center">
+        <span class="relative flex h-3 w-3">
+          <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+          <span class="relative inline-flex rounded-full h-3 w-3 bg-green-700"></span>
+        </span>
+        <div>All system normal</div>
+      </div>
+      <div className="flex items-center text-3xl gap-5">
+        {siteConfig.socials.map(({ link, icon: Icon }, i) => (
+          <a
+            key={i}
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white transition-colors"
+          >
+            <Icon />
+          </a>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/config/site.js
@@ -0,0 +1,26 @@
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+
+export const siteConfig = {
+  profile: {
+    name: "North",
+    description:
+      '<span class="text-[#9C9C9C]">Halo saya Arrom Adipta Bagas</span> Fresh Graduate D3 Akuntansi dengan 1+ tahun pengalaman serta keterampilan dalam memecahkan masalah keuangan dan akuntansi dengan membangun streamlining reports dengan data yang ternormalisasi serta terstruktur sehingga memberikan insight keuangan yang akurat. Memiliki dasar pemahaman yang kuat terhadap siklus akuntansi, dan perpajakan.',
+    cvUrl:
+      "https://drive.google.com/file/d/1le51bZIm4WRte7OFJ-1wqcX8GO2ANCy3/view?usp=sharing",
+  },
+  socials: [
+    {
+      link: "https://www.instagram.com/nrthgas.0/",
+      icon: FaInstagram,
+    },
+    {
+      link: "https://www.linkedin.com/in/adiptabagas/",
+      icon: FaLinkedin,
+    },
+    {
+      link: "https://github.com/afterhourss",
+      icon: IoLogoGithub,
+    },
+  ],
+};
